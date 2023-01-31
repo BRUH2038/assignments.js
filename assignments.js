695a9d138f8ad6e8dd3771824cdf6d9a990d1cd5
BRUH2038 <111803392+BRUH2038@users.noreply.github.com>
2023-01-31 13:23:41 -0700

    idk

--- assignments.js
+++ assignments.js
@@ -6,19 +6,4 @@ let your_country = "Mexico"
 let country2 = country
 
 
-<!DOCTYPE html>
-<html>
-<body>
-
-
-<h1>My First Web Page</h1>
-<p>My first paragraph.</p>
-
-
-<script>
-alert(5 + 6);
-</script>
-
-
-</body>
-</html>
+alert(country2)
