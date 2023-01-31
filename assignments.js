4c81301f3923eea8dfbee5e9993faf39755515ca
BRUH2038 <111803392+BRUH2038@users.noreply.github.com>
2023-01-31 13:15:47 -0700

    Update assignments.js

--- assignments.js
+++ assignments.js
@@ -6,4 +6,19 @@ let your_country = "Mexico"
 let country2 = country
 
 
-alert(country2)
+<!DOCTYPE html>
+<html>
+<body>
+
+
+<h1>My First Web Page</h1>
+<p>My first paragraph.</p>
+
+
+<script>
+alert(5 + 6);
+</script>
+
+
+</body>
+</html>
