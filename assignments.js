c20a289c10e4181391359044b2442963d3772bb7
BRUH2038 <111803392+BRUH2038@users.noreply.github.com>
2023-01-31 13:05:08 -0700

    L1 ASSIGNMENT: Variables and Values

--- assignments.js
+++ assignments.js
@@ -1,1 +1,9 @@
-alert("bruh")
+// JSF1 Assignment Variables and Values
+let country = "Mexico"
+let continent = "North America"
+let population = "129 million"
+let your_country = "Mexico"
+let country2 = country
+
+
+alert(country2)
