3f4e01fff7017fb0d1cd4c008ddb5fc0921199f3
BRUH2038 <111803392+BRUH2038@users.noreply.github.com>
2023-02-01 12:46:46 -0700

    JSF1 Assignment let, const, var

--- assignments.js
+++ assignments.js
@@ -1,6 +1,10 @@
 // JSF1 Assignment Variables and Values
-let country = "Mexico"
-let continent = "North America"
+
+
+const country = "Mexico"
+const continent = "North America"
+// let country = "Mexico"
+// let continent = "North America"
 let population = "129 million"
 let your_country = "Mexico"
 let country2 = country
@@ -8,4 +12,16 @@ let country2 = country
 
 alert(country2)
 
- // JSF1 Assignment Data Types
+// JSF1 Assignment Data Types
+
+let isIsland = false
+let language = ""
+console.log(isIsland)
+console.log(language)
+console.log(country)
+console.log(population)
+
+// JSF1 Assignment let, const, var
+language = "Spanish"
+
+// JSF1 Assignment Basic Operators
